43c1617d749e5e3411119d27fab0577330dd2d87
Kentaro Koshio (Woven Alpha)/越尾　健太郎 <kentaro.koshio@woven-planet.global>
2023-01-19 13:22:33 +0900

    initialize commit

---
+++ Assets/BallTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTransform : MonoBehaviour
+{
+    public Vector3 scaleChange;
+    public Vector3 positionChange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += scaleChange;
+        transform.position += positionChange;
+    }
+}
